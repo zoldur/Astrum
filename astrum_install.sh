539b2538d46a4fd3b0282d3694167443cd26635e
Zoldur <zoldur99@gmail.com>
2018-02-16 04:27:09 -0800

    Multiple MN on same VPS

--- astrum_install.sh
+++ astrum_install.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-DEFAULTASTRUMUSER="astrum"
-DEFAULTASTRUMPORT=25117
+TMP_FOLDER=$(mktemp -d)
+CONFIG_FILE="astrum.conf"
+ASTRUM_DAEMON="Astrumd"
+ASTRUM_REPO="https://github.com/astrumcash/astrum"
+
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'
+
 
 function compile_error() {
 if [ "$?" -gt "0" ];
@@ -19,20 +27,26 @@ if [[ $(lsb_release -d) != *16.04* ]]; then
 fi
 
 if [[ $EUID -ne 0 ]]; then
-   echo -e "${RED}$0 must be run as root.${NC}" 
+   echo -e "${RED}$0 must be run as root.${NC}"
    exit 1
 fi
 
-if [ -n "$(pidof astrumcoind)" ]; then
-  echo -e "${GREEN}AstrumCash already running.${NC}"
-  exit 1
+if [ -n "$(pidof $ASTRUM_DAEMON)" ]; then
+  echo -e "${GREEN}\c"
+  read -e -p "AstrumCash is already running. Do you want to add another MN? [Y/N]" NEW_ASTRUM
+  echo -e "{NC}"
+  clear
+else
+  NEW_ASTRUM="new"
 fi
 }
 
 function prepare_system() {
 
-echo -e "Prepare the system to install AstrumCash Master Node."
+echo -e "Prepare the system to install Astrum master node."
 apt-get update >/dev/null 2>&1
+DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
+DEBIAN_FRONTEND=noninteractive apt-get upgrade -y -qq > /dev/null 2>&1
 apt install -y software-properties-common >/dev/null 2>&1
 echo -e "${GREEN}Adding bitcoin PPA repository"
 apt-add-repository -y ppa:bitcoin/bitcoin >/dev/null 2>&1
@@ -40,18 +54,18 @@ echo -e "Installing required packages, it may take some time to finish.${NC}"
 apt-get update >/dev/null 2>&1
 apt-get install -y make software-properties-common build-essential libtool autoconf libssl-dev libboost-dev libboost-chrono-dev \
 libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git \
-wget pwgen curl libdb4.8-dev bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp-dev
+wget pwgen curl libdb4.8-dev bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev
 clear
 if [ "$?" -gt "0" ];
   then
     echo -e "${RED}Not all required packages were installed properly. Try to install them manually by running the following commands:${NC}\n"
-        echo "apt-get update"
+    echo "apt-get update"
     echo "apt -y install software-properties-common"
     echo "apt-add-repository -y ppa:bitcoin/bitcoin"
     echo "apt-get update"
     echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
 libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git pwgen curl libdb4.8-dev \
-bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp-dev" 
+bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev"
  exit 1
 fi
 
@@ -71,11 +85,11 @@ fi
 clear
 }
 
+function compile_astrum() {
+  echo -e "Clone git repo and compile it. This may take some time. Press a key to continue."
+  read -n 1 -s -r -p ""
 
-function compile_astrumcoin() {
-echo -e "Clone git repo and compile it. This may take some time. Press a key to continue."
-read -n 1 -s -r -p ""
-
+  cd $TMP_FOLDER
   git clone https://github.com/bitcoin-core/secp256k1
   cd ~/secp256k1
   chmod +x ./autogen.sh
@@ -85,127 +99,185 @@ read -n 1 -s -r -p ""
   compile_error secp256k1
   ./tests
   sudo make install
+  clear
+
   cd ~
-  git clone https://github.com/astrumcash/astrum
+  git clone $ASTRUM_REPO
   cd ~/astrum/src
   make -f makefile.unix # Headless
-  compile_error Astraumcash
-  cp -a Astrumd /usr/local/bin
+  compile_error AstraumCash
+  cp -a $ASTRUM_DAEMON /usr/local/bin
   clear
 }
 
 function enable_firewall() {
-FWSTATUS=$(ufw status 2>/dev/null|awk '/^Status:/{print $NF}')
-if [ "$FWSTATUS" = "active" ]; then
-  echo -e "Setting up firewall to allow ingress on port ${GREEN}$ASTRUMPORT${NC}"
-  ufw allow $ASTRUMPORT/tcp comment "Astrum MN port" >/dev/null
-fi
+  FWSTATUS=$(ufw status 2>/dev/null|awk '/^Status:/{print $NF}')
+  if [ "$FWSTATUS" = "active" ]; then
+    echo -e "Setting up firewall to allow ingress on port ${GREEN}$ASTRUMPORT${NC}"
+    ufw allow $ASTRUMPORT/tcp comment "Astrum MN port" >/dev/null
+  fi
 }
 
 function systemd_astrum() {
-
-cat << EOF > /etc/systemd/system/Astrumd.service
+  cat << EOF > /etc/systemd/system/$ASTRUMUSER.service
 [Unit]
-Description=AstrumD service
+Description=Astrum service
 After=network.target
+
 [Service]
-ExecStart=/usr/local/bin/Astrumd -conf=$ASTRUMFOLDER/Astrum.conf -datadir=$ASTRUMFOLDER
-ExecStop=/usr/local/bin/Astrumd -conf=$ASTRUMFOLDER/Astrum.conf -datadir=$ASTRUMFOLDER stop
-Restart=on-abort
+Type=forking
+ExecStart=$ASTRUM_DAEMON -conf=$ASTRUMFOLDER/$CONFIG_FILE -datadir=$ASTRUMFOLDER
+ExecStop=$ASTRUM_DAEMON -conf=$ASTRUMFOLDER/$CONFIG_FILE -datadir=$ASTRUMFOLDER stop
+Restart=always
+RestartSec=5
+PrivateTmp=true
+TimeoutStopSec=60s
+TimeoutStartSec=5s
+StartLimitInterval=120s
+StartLimitBurst=15
 User=$ASTRUMUSER
 Group=$ASTRUMUSER
+
 [Install]
 WantedBy=multi-user.target
 EOF
+
+  systemctl daemon-reload
+  sleep 3
+  systemctl start $ASTRUMUSER.service
+  systemctl enable $ASTRUMUSER.service
+
+  if [[ -z "$(ps axo user:15,cmd:100 | egrep ^$ASTRUMUSER | grep $ASTRUM_DAEMON)" ]]; then
+    echo -e "${RED}Astrumd is not running${NC}, please investigate. You should start by running the following commands as root:"
+    echo "systemctl start $ASTRUMUSER.service"
+    echo "systemctl status $ASTRUMUSER.service"
+    echo "less /var/log/syslog"
+    exit 1
+  fi
 }
+
+function ask_port() {
+DEFAULTASTRUMPORT=17720
+read -p "ASTRUM Port: " -i $DEFAULTASTRUMPORT -e ASTRUMPORT
+: ${ASTRUMPORT:=$DEFAULTASTRUMPORT}
+}
+
+function ask_user() {
+  DEFAULTASTRUMUSER="astrum"
+  read -p "Astrum user: " -i $DEFAULTASTRUMUSER -e ASTRUMUSER
+  : ${ASTRUMUSER:=$DEFAULTASTRUMUSER}
+
+  if [ -z "$(getent passwd $ASTRUMUSER)" ]; then
+    USERPASS=$(pwgen -s 12 1)
+    useradd -m $ASTRUMUSER
+    echo "$ASTRUMUSER:$USERPASS" | chpasswd
+
+    ASTRUMHOME=$(sudo -H -u $ASTRUMUSER bash -c 'echo $HOME')
+    DEFAULTASTRUMFOLDER="$ASTRUMHOME/.astrum"
+    read -p "Configuration folder: " -i $DEFAULTASTRUMFOLDER -e ASTRUMFOLDER
+    : ${ASTRUMFOLDER:=$DEFAULTASTRUMFOLDER}
+    mkdir -p $ASTRUMFOLDER
+    chown -R $ASTRUMUSER: $ASTRUMFOLDER >/dev/null
+  else
+    clear
+    echo -e "${RED}User exits. Please enter another username: ${NC}"
+    ask_user
+  fi
+}
+
+function check_port() {
+  declare -a PORTS
+  PORTS=($(netstat -tnlp | awk '/LISTEN/ {print $4}' | awk -F":" '{print $NF}' | sort | uniq | tr '\r\n'  ' '))
+  ask_port
+
+  while [[ ${PORTS[@]} =~ $ASTRUMPORT ]] || [[ ${PORTS[@]} =~ $[ASTRUMPORT+1] ]]; do
+    clear
+    echo -e "${RED}Port in use, please choose another port:${NF}"
+    ask_port
+  done
+}
+
+function create_config() {
+  RPCUSER=$(pwgen -s 8 1)
+  RPCPASSWORD=$(pwgen -s 15 1)
+  cat << EOF > $ASTRUMFOLDER/$CONFIG_FILE
+rpcuser=$RPCUSER
+rpcpassword=$RPCPASSWORD
+rpcallowip=127.0.0.1
+rpcport=$[ASTRUMPORT+1]
+listen=1
+server=1
+daemon=1
+port=$ASTRUMPORT
+EOF
+}
+
+function create_key() {
+  echo -e "Enter your ${RED}Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
+  read -e ASTRUMKEY
+  if [[ -z "$ASTRUMKEY" ]]; then
+  sudo -u $ASTRUMUSER $ASTRUM_DAEMON -conf=$ASTRUMFOLDER/$CONFIG_FILE -datadir=$ASTRUMFOLDER
+  sleep 5
+  if [ -z "$(ps axo user:15,cmd:100 | egrep ^$ASTRUMUSER | grep $ASTRUM_DAEMON)" ]; then
+   echo -e "${RED}Astrumd server couldn't start. Check /var/log/syslog for errors.{$NC}"
+   exit 1
+  fi
+  ASTRUMKEY=$(sudo -u $ASTRUMUSER $ASTRUM_DAEMON -conf=$ASTRUMFOLDER/$CONFIG_FILE -datadir=$ASTRUMFOLDER masternode genkey)
+  sudo -u $ASTRUMUSER $ASTRUM_DAEMON -conf=$ASTRUMFOLDER/$CONFIG_FILE -datadir=$ASTRUMFOLDER stop
+fi
+}
+
+function update_config() {
+  sed -i 's/daemon=1/daemon=0/' $ASTRUMFOLDER/$CONFIG_FILE
+  NODEIP=$(curl -s4 icanhazip.com)
+  cat << EOF >> $ASTRUMFOLDER/$CONFIG_FILE
+logtimestamps=1
+maxconnections=256
+masternode=1
+masternodeaddr=$NODEIP
+masternodeprivkey=$ASTRUMKEY
+EOF
+  chown -R $ASTRUMUSER: $ASTRUMFOLDER >/dev/null
+}
+
+function important_information() {
+ echo
+ echo -e "================================================================================================================================"
+ echo -e "Astrum Masternode is up and running as user ${GREEN}$ASTRUMUSER${NC} and it is listening on port ${GREEN}$ASTRUMPORT${NC}."
+ echo -e "${GREEN}$ASTRUMUSER${NC} password is ${RED}$USERPASS${NC}"
+ echo -e "Configuration file is: ${RED}$ASTRUMFOLDER/$CONFIG_FILE${NC}"
+ echo -e "Start: ${RED}systemctl start $ASTRUMUSER.service${NC}"
+ echo -e "Stop: ${RED}systemctl stop $ASTRUMUSER.service${NC}"
+ echo -e "VPS_IP:PORT ${RED}$NODEIP:$ASTRUMPORT${NC}"
+ echo -e "MASTERNODE PRIVATEKEY is: ${RED}$ASTRUMKEY${NC}"
+ echo -e "================================================================================================================================"
+}
+
+function setup_node() {
+  ask_user
+  check_port
+  create_config
+  create_key
+  update_config
+  enable_firewall
+  systemd_astrum
+  important_information
+}
+
 
 ##### Main #####
 clear
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m'
-
 checks
-prepare_system
-compile_astrumcoin
-
-
-echo -e "${GREEN}Prepare to configure and start AstrumCash Masternode.${NC}"
-
-read -p "AstrumCash user: " -i $DEFAULTASTRUMUSER -e ASTRUMUSER
-: ${ASTRUMUSER:=$DEFAULTASTRUMUSER}
-useradd -m $ASTRUMUSER >/dev/null
-ASTRUMHOME=$(sudo -H -u $ASTRUMUSER bash -c 'echo $HOME')
-
-DEFAULTASTRUMFOLDER="$ASTRUMHOME/.Astrum"
-read -p "Configuration folder: " -i $DEFAULTASTRUMFOLDER -e ASTRUMFOLDER
-: ${ASTRUMFOLDER:=$DEFAULTASTRUMFOLDER}
-mkdir -p $ASTRUMFOLDER
-
-RPCUSER=$(pwgen -s 8 1)
-RPCPASSWORD=$(pwgen -s 15 1)
-cat << EOF > $ASTRUMFOLDER/Astrum.conf
-rpcuser=$RPCUSER
-rpcpassword=$RPCPASSWORD
-rpcallowip=127.0.0.1
-listen=1
-server=1
-daemon=1
-EOF
-chown -R $ASTRUMUSER $ASTRUMFOLDER >/dev/null
-
-
-read -p "ASTRUM Port: " -i $DEFAULTASTRUMPORT -e ASTRUMPORT
-: ${ASTRUMPORT:=$DEFAULTASTRUMPORT}
-:wq
-echo -e "Enter your ${RED}Masternode Private Key${NC}. Leave it blank to generate a new ${RED}Masternode Private Key${NC} for you:"
-read -e ASTRUMKEY
-if [[ -z "$ASTRUMKEY" ]]; then
- sudo -u $ASTRUMUSER /usr/local/bin/Astrumd -conf=$ASTRUMFOLDER/Astrum.conf -datadir=$ASTRUMFOLDER
- sleep 5
- if [ -z "$(pidof Astrumd)" ]; then
-   echo -e "${RED}AstrumCash server couldn't start. Check /var/log/syslog for errors.{$NC}"
-   exit 1
- fi
- ASTRUMKEY=$(sudo -u $ASTRUMUSER /usr/local/bin/Astrumd -conf=$ASTRUMFOLDER/Astrum.conf -datadir=$ASTRUMFOLDER masternode genkey)
- kill $(pidof Astrumd)
+if [[ ("$NEW_ASTRUM" == "y" || "$NEW_ASTRUM" == "Y") ]]; then
+  setup_node
+  exit 0
+elif [[ "$NEW_ASTRUM" == "new" ]]; then
+  prepare_system
+  compile_astrum
+  setup_node
+else
+  echo -e "${GREEN}Astrumd already running.${NC}"
+  exit 0
 fi
-
-sed -i 's/daemon=1/daemon=0/' $ASTRUMFOLDER/Astrum.conf
-NODEIP=$(curl -s4 icanhazip.com)
-cat << EOF >> $ASTRUMFOLDER/Astrum.conf
-maxconnections=256
-masternode=1
-masternodeprivkey=$ASTRUMKEY
-masternodeaddr=$NODEIP:$ASTRUMPORT
-EOF
-chown -R $ASTRUMUSER: $ASTRUMFOLDER >/dev/null
-
-
-systemd_astrum
-enable_firewall
-
-
-systemctl daemon-reload
-sleep 3
-systemctl start Astrumd.service
-systemctl enable Astrumd.service
-
-
-if [[ -z $(pidof Astrumd) ]]; then
-  echo -e "${RED}AstrumCash is not running${NC}, please investigate. You should start by running the following commands as root:"
-  echo "systemctl start Astrumd.service"
-  echo "systemctl status Astrumd.service"
-  echo "less /var/log/syslog"
-  exit 1 
-fi
-
-echo
-echo -e "======================================================================================================================="
-echo -e "AstrumCashh Masternode is up and running as user ${GREEN}$ASTRUMUSER${NC} and it is listening on port ${GREEN}$ASTRUMPORT${NC}." 
-echo -e "Configuration file is: ${RED}$ASTRUMFOLDER/astrumcoin.conf${NC}"
-echo -e "VPS_IP:PORT ${RED}$NODEIP:$ASTRUMPORT${NC}"
-echo -e "MASTERNODE PRIVATEKEY is: ${RED}$ASTRUMKEY${NC}"
-echo -e "========================================================================================================================"
 
